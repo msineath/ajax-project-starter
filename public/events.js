95f0690e558d6027d5a941c4b5136a175372c5fe
msineath <msineath@LAPTOP-65OGSQM1.localdomain>
2021-03-11 16:06:51 -0500

    finished upvote

--- public/events.js
+++ public/events.js
@@ -1,17 +1,50 @@
 // const fetch = require ('node-fetch')
 const getCatPic = async () => {
+    let loader = document.querySelector(".loader");
+    loader.innerHTML = "Loading...";
     const res = await fetch(`/kitten/image`);
     const json = await res.json();
     let catPic = document.getElementsByClassName('cat-pic');
-    console.log(catPic)
+    // console.log(catPic)
 
     if(res.ok) {
-        console.log(json)
+        // console.log(json)
         catPic[0].src = json.src;
-        console.log(catPic)
+        // console.log(catPic)
+        loader.innerHTML = '';
+    } else {
+        alert("Something went wrong, please try again...")
     }
 
 }
 
 
 window.addEventListener('DOMContentLoaded', getCatPic)
+let newPic = document.getElementById('new-pic');
+newPic.addEventListener('click', getCatPic);
+
+let upvote = document.getElementById('upvote');
+let downvote = document.getElementById('downvote');
+let score = document.querySelector('.score');
+
+const upVote = async () => {
+    const res = await fetch(`/kitten/upvote`, {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json'            
+        },
+        body: JSON.stringify({score})
+    });
+       const json = await res.json();
+       console.log(res);
+       console.log(json) 
+
+   if(res.ok) {
+       console.log(score)
+    score.innerHTML = json.score;
+   } else {
+       alert('Something went wrong, please try again...');
+   }
+}
+
+upvote.addEventListener('click', upVote)
